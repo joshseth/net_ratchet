438cb93a8b57a7dc6fa13f2371cbb191b70dff87
peter <petrel.harp@gmail.com>
2018-06-07 14:31:42 -0700

    added dimensions as arg to gene deletion

--- do_gene_deletions.R
+++ do_gene_deletions.R
@@ -1,16 +1,20 @@
 #!/usr/bin/env Rscript
 
 usage <- "
-    ./do_gene_deletions.R (name of directory with params.R in)
+    ./do_gene_deletions.R (name of directory with params.R in) (number of extra dimensions)
 "
 
 args <- if (interactive()) { scan(what='') } else { commandArgs(TRUE) }
-if (length(args) != 1) {
+if (length(args) != 2) {
     stop(usage)
 }
 
 basedir <- args[1]
+extra_dims <- as.integer(args[2])
 paramfile <- file.path(basedir, "params.R")
+outdir <- file.path(basedir, "deletion_phenotypes")
+dir.create(outdir, showWarnings=FALSE)
+outfile <- file.path(outdir, sprintf("deletion_phenotypes.dims_%d.pdf", extra_dims))
 
 if (!file.exists(paramfile)) {
     stop(paste("Parameter file", paramfile, "does not exist."))
@@ -30,11 +34,10 @@ if (!exists("sys0")) {
 systems <- list()
 sf <- rep(0,50)
 system_sigma <- 0.001
-system_size <- 8
-sys1 <- rand_realization(sys0, system_sigma, system_size)
+sys1 <- rand_realization(sys0, system_sigma, extra_dims)
 max_time <- 10
 
-for (j in 1:10)
+for (j in 1:nrow(sys1$A))
 {
     dsys1 <- delete_gene(sys1, j)
     systems[[j]] <- dsys1
@@ -42,13 +45,12 @@ for (j in 1:10)
     sf[j] <- D(dsys1$A, dsys1$B, dsys1$C, sys0$optimal_h)
 }
 
-pdf(file=file.path(basedir, "deletion_phenotypes.pdf"), 
-    width=8, height=6, pointsize=10)
+pdf(file=outfile, width=8, height=6, pointsize=10)
 
-plot_many_phenotypes(systems, score=sf, max_time=max_time,
-                 main=sprintf("deletions : %s", basedir))
-mtext(sprintf("system sigma=%0.4f, size=%d",
-              system_sigma, system_size ), side=3, line=0.2)
+    plot_many_phenotypes(systems, score=sf, max_time=max_time,
+                     main=sprintf("deletions : %s", basedir))
+    mtext(sprintf("system sigma=%0.4f, extra dims=%d",
+                  system_sigma, extra_dims), side=3, line=0.2)
 
 dev.off()
 
--- network_fns.R
+++ network_fns.R
@@ -2,6 +2,7 @@ library(Matrix)
 library(expm)
 
 rand_realization <- function (sys, std, m) {
+    # m is the number of extra dimensions
     n0 <- nrow(sys$A);
     bn0 <- ncol(sys$B);
     cn0 <- nrow(sys$C);
@@ -9,24 +10,16 @@ rand_realization <- function (sys, std, m) {
     A22 <- sys$A;
     B2 <- sys$B;
     C2 <- sys$C;
-    if(is.null(m))
-    {
-        m <- 0;
-    }
     
     if (m != 0)
     {
         A21 <- matrix(c(rep(0,n0*m)), nrow=n0, ncol=m);
         A12 <- matrix(c(rnorm(m*n0, mean=0, std)), nrow=m, ncol=n0);
         A11 <- matrix(c(rnorm(m^2, mean=0, std)), nrow=m, ncol=m);
-        
         #B2 <- matrix(c(rnorm(m*bn0, mean=0, sd=std)), nrow = m, ncol = bn0 );
         B1 <- matrix(c(rep(0,m*bn0)), nrow = m, ncol = bn0 );
         C1 <- matrix(c(rep(0, cn0*m)), nrow = cn0, ncol = m);
-     }
-
-    if (m == 0)
-    {
+    } else {
         A11 <- NULL;
         A12 <- NULL;
         A21 <- NULL;
@@ -40,7 +33,8 @@ rand_realization <- function (sys, std, m) {
     KalC <- cbind(C1, C2);
     
     #### Generate P, the change of basis matrix #################
-    P <- matrix(c(rnorm((n0+m)^2, mean=0, sd=std)), nrow = (n0 + m), ncol = (n0 + m));
+    P <- matrix(c(rnorm((n0+m)^2, mean=0, sd=std)), 
+                nrow = (n0 + m), ncol = (n0 + m));
 
     #### Rescale and replace eigenvalues of P ###
     eig <- eigen(P)
@@ -60,28 +54,12 @@ rand_realization <- function (sys, std, m) {
 
 delete_gene <- function(sys, d)
 {
-    #d 'is gene to be deleted
-    #n is dimension of A matrix
-    n <- nrow(sys$A)
-    del <- matrix(0, nrow = (n-1), ncol = n )
-
-    for (i in 1:n)
-    {
-        if (i < d)
-        {
-            del[i,i] <- 1
-        }
-        if (i > d)
-        {
-            del[i-1,i] <- 1
-        }
-    }
-    
-    del_sys <- list()
-    del_sys$A <- del%*%sys$A%*%t(del);
-    del_sys$B <- del%*%sys$B;
-    del_sys$C <- sys$C%*%t(del);
-
+    # d is gene to be deleted
+    # n is dimension of A matrix
+    stopifnot(d <= nrow(sys$A))
+    del_sys <- list(A = sys$A[-d, -d, drop=FALSE],
+                    B = sys$B[-d, , drop=FALSE],
+                    C = sys$C[, -d, drop=FALSE])
     return(del_sys)
 }
 
