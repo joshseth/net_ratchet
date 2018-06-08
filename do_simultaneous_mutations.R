8f7c6e2e3662a53ae939f9d79501159eea614a8a
Josh <jsschiff@usc.edu>
2018-06-08 11:20:41 -0700

    edited simultaneous mutation code

--- do_simultaneous_mutations.R
+++ do_simultaneous_mutations.R
@@ -1,16 +1,20 @@
 #!/usr/bin/env Rscript
 
 usage <- "
-    ./do_simultaneous_mutations.R (name of directory with params.R in)
+    ./do_simultaneous_mutations.R (name of directory with params.R in) (number of extra dimensions)
 "
 
 args <- if (interactive()) { scan(what='') } else { commandArgs(TRUE) }
-if (length(args) != 1) {
+if (length(args) != 2) {
     stop(usage)
 }
 
 basedir <- args[1]
+extra_dims <- as.integer(args[2])
 paramfile <- file.path(basedir, "params.R")
+outdir <- file.path(basedir, "simultaneous_mutations_phenotypes")
+dir.create(outdir, showWarnings=FALSE)
+outfile <- file.path(outdir, sprintf("simultaneous_mutations_phenotypes.dims_%d.pdf", extra_dims))
 
 source("network_fns.R")
 source("plotting_fns.R")
@@ -26,26 +30,24 @@ if (!exists("sys0")) {
 systems <- list()
 sf <- rep(0,50)
 system_sigma <- 0.001
-system_size <- 8
-sys1 <- rand_realization(sys0, system_sigma, system_size)
+sys1 <- rand_realization(sys0, system_sigma, extra_dims)
 max_time <- 10
 mutation_sigma <- 0.01
 
 for (j in 1:50)
 {
     dsys1 <- sys1
-    dsys1$A <- sys1$A + rnorm(10^2, 0, mutation_sigma)
+    dsys1$A <- sys1$A + rnorm(nrow(sys1$A)^2, 0, mutation_sigma)
     systems[[j]] <- dsys1
     sf[j] <- D(dsys1$A, dsys1$B, dsys1$C, sys0$optimal_h)
 }
 
-pdf(file=file.path(basedir, "simultaneous_mutation_phenotypes.pdf"), 
-    width=8, height=4, pointsize=10)
+pdf(file=outfile, width=8, height=4, pointsize=10)
 
 plot_many_phenotypes(systems, sf, max_time=max_time,
                  main=sprintf("simultaneous mutations : %s", basedir))
 mtext(sprintf("mutation sigma=%0.4f, system sigma=%0.4f, size=%d",
-              mutation_sigma, system_sigma, system_size ), side=3, line=0.2)
+              mutation_sigma, system_sigma, extra_dims ), side=3, line=0.2)
 
 dev.off()
 
