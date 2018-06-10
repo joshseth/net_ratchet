6021c2e8a564ce6117c1916d193384e73c45136d
Josh <jsschiff@usc.edu>
2018-06-10 16:08:45 -0700

    evolutionary simulation code

--- evo_kal.R
+++ evo_kal.R
@@ -2,7 +2,7 @@ source("network_fns.R")
 
 evolve <- function(sys0) {
   population_size <- 100
-  max_generation <- 100
+  max_generation <- 1000
   pop <- rep(list(sys0), population_size)
   next_gen <- rep(list(sys0), population_size)
 
@@ -14,14 +14,14 @@ for (generations in 1:max_generation)
     # mutate coefficients
     for (k in 1:length(pop[[i]]$A))
     {
-      if ( sample(1:100,1) == 1)
+      if ( sample(1:1000,1) == 1)
       {
         pop[[i]]$A[k] <- pop[[i]]$A[k] + rnorm(1,0,1)
     }
     }
     for (k in 1:length(pop[[i]]$B))
     {
-      if ( sample(1:100,1) == 1)
+      if ( sample(1:1000,1) == 1)
       {
         pop[[i]]$B[k] <- pop[[i]]$B[k] + rnorm(1,0,1)
     }
@@ -35,13 +35,13 @@ for (generations in 1:max_generation)
     }
 
     # gene deletions
-    if ( sample(1:100,1) == 1)
+    if ( sample(1:10000,1) == 1 & nrow(pop[[i]]$A > 1))
     {
       d <- sample(1:nrow(pop[[i]]$A), 1)
       pop[[i]] <- delete_gene(pop[[i]], d)
     }
     # gene duplications
-    if (sample(1:100,1) == 1)
+    if (sample(1:1000,1) == 1)
     {
       pop[[i]]$A <- rbind(cbind(pop[[i]]$A, matrix(rep(0, nrow(pop[[i]]$A)), nrow= nrow(pop[[i]]$A), ncol=1)), matrix(0, nrow = 1, ncol = ncol(pop[[i]]$A) + 1))
       pop[[i]]$B <- rbind(pop[[i]]$B, 0)
@@ -53,18 +53,18 @@ for (generations in 1:max_generation)
   sorted_pop <- pop[order(sapply(pop, '[[', 'fitness'), decreasing = TRUE)]
   list_of_fitnesses <- sapply(sorted_pop, '[[', 'fitness')
   sum_of_fitnesses <- sum(sapply(sorted_pop, '[[', 'fitness'))
-#  if (sum_of_fitnesses > 10^-6)
-#  {
-#  brackets <- rep(0, population_size + 1)
-#  for (i in 1:population_size)
-#  {
-#    brackets[i+1] <- sum(list_of_fitnesses[1:i]/sum(list_of_fitnesses))
-#  }
-#  }
- # if (sum_of_fitnesses <= 10^-6)
- # {
+  if (sum_of_fitnesses > 10^-6)
+  {
+  brackets <- rep(0, population_size + 1)
+  for (i in 1:population_size)
+  {
+    brackets[i+1] <- sum(list_of_fitnesses[1:i]/sum(list_of_fitnesses))
+  }
+  }
+  if (sum_of_fitnesses <= 10^-6)
+  {
     brackets <- seq(0,1, 1/population_size)
- # }
+  }
 
   for (j in 1:population_size)
   {
